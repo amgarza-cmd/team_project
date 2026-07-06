aeffc92840adf3e41ede56b7d9c09cf7fd677a80
MBarahona388 <mbarahonarivera@islander.tamucc.edu>
2026-07-05 19:56:45 -0600

    Add team page

---
+++ src/pages/About.jsx
@@ -0,0 +1,14 @@
+export default function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This page contains basic team information for the project.</p>
+
+      <ul>
+        <li>Marcelo - Team member</li>
+        <li>CarvingRiper386 - Team member</li>
+        <li>timothyjg123 - Team member</li>
+      </ul>
+    </div>
+  );
+}
